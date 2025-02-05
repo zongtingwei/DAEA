7df1798f8cc869b7d0b7e0d74bff8dd2aedb24c3
zongtingwei <20228131062@m.scnu.edu.cn>
2025-02-05 11:44:58 +0800

    Update main_DAEA.m

--- main_DAEA.m
+++ main_DAEA.m
@@ -10,7 +10,7 @@ clear;
 tic;
 
 algorithmName = 'DAEA';  
-dataNameArray = {'colon'}; % data set
+dataNameArray = {'colon'}; % dataset
 global maxFES
 maxFES = 100;  % max number of iteration
 global choice
